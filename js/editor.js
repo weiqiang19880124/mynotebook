3adce25e92ed989b1656cae7dc5527fea3d1146f
rushmoon <hialanzhd@sina.com>
2018-08-14 16:46:18 +0800

    实现笔记浏览，增删改查的基本操作，还未实现文件夹管理

--- js/editor.js
+++ js/editor.js
@@ -1,7 +1,13 @@
 angular.module('App')
 .controller('EditController',function ($scope,$http) {
     $scope.editing = true;
-
+    $http.get('/notes')
+        .success(function (data) {  //$http.get()加载笔记
+            $scope.notes = data; //返回数据赋值给$scope.notes
+        })
+        .error(function (err) {
+            $scope.error = '无法加载笔记';
+        });
     $scope.view = function (index) {
         $scope.editing = false;
         $scope.content = $scope.notes[index];
@@ -30,7 +36,26 @@ angular.module('App')
                $scope.error = 'Could not create note';
            });
          }
-    }
+    };
+    $scope.remove = function () {
+        var found = -1;
+        $http.delete('/notes/'+$scope.content.id).success(function () {
+            angular.forEach($scope.notes,function(note,index){
+                if (note.id === $scope.content.id){
+                    found = index;
+                }
+            });
+            if (found >=0){
+                $scope.notes.splice(found, 1);
+            }
+            $scope.content = {
+                title:'',
+                content:''
+            }
+        }).error(function () {
+            $scope.error = '无法删除该条笔记';
+        })
+    };
 });
 // $scope.create = function () {
 //     $scope.editing = true;
